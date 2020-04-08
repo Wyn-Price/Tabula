70d19d5e0dc3cbb0a2164e2456d7fbf9cd23e003
wynprice999@gmail.com <wynprice999@gmail.com>
2020-04-08 23:08:18 +0100

    Added dragging of texture parts

--- src/main/java/me/ichun/mods/tabula/client/gui/window/WindowTexture.java
+++ src/main/java/me/ichun/mods/tabula/client/gui/window/WindowTexture.java
@@ -15,6 +15,8 @@ import me.ichun.mods.tabula.client.gui.Theme;
 import me.ichun.mods.tabula.client.mainframe.core.ProjectHelper;
 import me.ichun.mods.tabula.common.Tabula;
 import me.ichun.mods.tabula.common.packet.PacketClearTexture;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
@@ -22,11 +24,16 @@ import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.translation.I18n;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class WindowTexture extends Window
 {
@@ -34,6 +41,13 @@ public class WindowTexture extends Window
 
     public BufferedImage image;
     public int imageId = -1;
+
+    private int lastMouseX = -1;
+    private int lastMouseY = -1;
+
+    private boolean lastMouseDown;
+    private String startingMouseDown = "";
+    private Set<String> selectedIdentifiers = new HashSet<>();
 
     public WindowTexture(IWorkspace parent, int x, int y, int w, int h, int minW, int minH)
     {
@@ -48,6 +62,9 @@ public class WindowTexture extends Window
     public void draw(int mouseX, int mouseY) //4 pixel border?
     {
         super.draw(mouseX, mouseY);
+
+
+
         if(!((GuiWorkspace)workspace).projectManager.projects.isEmpty() && !minimized)
         {
             ProjectInfo project = ((GuiWorkspace)workspace).projectManager.projects.get(((GuiWorkspace)workspace).projectManager.selectedProject);
@@ -64,6 +81,14 @@ public class WindowTexture extends Window
             double pY = posY + offY + 15;
             double w1 = (project.textureWidth * max);
             double h1 = (project.textureHeight * max);
+
+            double ratio = (project.textureWidth / w1);
+
+            int gMx = mouseX + posX;
+            int gMy = mouseY + posY;
+
+            double diffX = (mouseX - lastMouseX) * ratio;
+            double diffY = (mouseY - lastMouseY) * ratio;
 
             RendererHelper.drawColourOnScreen(200, 200, 200, 255, pX, pY, w1, h1, 0D);
 
@@ -88,20 +113,109 @@ public class WindowTexture extends Window
 
             GlStateManager.alphaFunc(GL11.GL_GREATER, 0.00625F);
 
+            boolean currentMouse = Mouse.isButtonDown(0);
+            if(!currentMouse) {
+                startingMouseDown = "";
+            }
+            boolean mouseDown = !currentMouse && lastMouseDown;
+            boolean mouseOverAny = false;
+
             for(ElementListTree.Tree tree : ((GuiWorkspace)workspace).windowModelTree.modelList.trees)
             {
                 if(tree.attachedObject instanceof CubeInfo)
                 {
                     CubeInfo info = (CubeInfo)tree.attachedObject;
                     int alpha = tree.selected ? 125 : 25;
-                    double ratio = (project.textureWidth / w1);
-                    RendererHelper.drawColourOnScreen(255, 0, 0, alpha, pX + info.txOffset[0] / ratio, pY + info.txOffset[1] / ratio + info.dimensions[2] / ratio                                                                                               , info.dimensions[2] / ratio, info.dimensions[1] / ratio, 0D);
-                    RendererHelper.drawColourOnScreen(0, 0, 255, alpha, pX + info.txOffset[0] / ratio + info.dimensions[2] / ratio, pY + info.txOffset[1] / ratio + info.dimensions[2] / ratio                                                                  , info.dimensions[0] / ratio, info.dimensions[1] / ratio, 0D);
-                    RendererHelper.drawColourOnScreen(170, 0, 0, alpha, pX + info.txOffset[0] / ratio + info.dimensions[2] / ratio + info.dimensions[0] / ratio, pY + info.txOffset[1] / ratio + info.dimensions[2] / ratio                                     , info.dimensions[2] / ratio, info.dimensions[1] / ratio, 0D);
-                    RendererHelper.drawColourOnScreen(0, 0, 170, alpha, pX + info.txOffset[0] / ratio + info.dimensions[2] / ratio + info.dimensions[0] / ratio  + info.dimensions[2] / ratio, pY + info.txOffset[1] / ratio + info.dimensions[2] / ratio       , info.dimensions[0] / ratio, info.dimensions[1] / ratio, 0D);
-                    RendererHelper.drawColourOnScreen(0, 255, 0, alpha, pX + info.txOffset[0] / ratio + info.dimensions[2] / ratio, pY + info.txOffset[1] / ratio                                                                                               , info.dimensions[0] / ratio, info.dimensions[2] / ratio, 0D);
-                    RendererHelper.drawColourOnScreen(0, 170, 0, alpha, pX + info.txOffset[0] / ratio + info.dimensions[2] / ratio + info.dimensions[0] / ratio, pY + info.txOffset[1] / ratio                                                                  , info.dimensions[0] / ratio, info.dimensions[2] / ratio, 0D);
+
+                    float rMod = 1F;
+                    float gMod = 1F;
+                    float bMod = 1F;
+
+                    if(selectedIdentifiers.contains(info.identifier)) {
+                        rMod = 1F;
+                        gMod = 0.5F;
+                        bMod = 0.6F;
+                        alpha = 125;
+                        if(currentMouse) {
+                            info.txOffset[0] += diffX;
+                            info.txOffset[1] += diffY;
+                        }
+                    }
+
+                    double[] posXs = new double[] {
+                        pX+info.txOffset[0] / ratio,
+                        pX+info.txOffset[0] / ratio + info.dimensions[2] / ratio,
+                        pX+info.txOffset[0] / ratio + info.dimensions[2] / ratio + info.dimensions[0] / ratio,
+                        pX+info.txOffset[0] / ratio + info.dimensions[2] / ratio + info.dimensions[0] / ratio  + info.dimensions[2] / ratio,
+                        pX+info.txOffset[0] / ratio + info.dimensions[2] / ratio,
+                        pX+info.txOffset[0] / ratio + info.dimensions[2] / ratio + info.dimensions[0] / ratio
+                    };
+
+                    double[] posYs = new double[] {
+                        pY+info.txOffset[1] / ratio + info.dimensions[2] / ratio,
+                        pY+info.txOffset[1] / ratio + info.dimensions[2] / ratio,
+                        pY+info.txOffset[1] / ratio + info.dimensions[2] / ratio,
+                        pY+info.txOffset[1] / ratio + info.dimensions[2] / ratio,
+                        pY+info.txOffset[1] / ratio,
+                        pY+info.txOffset[1] / ratio
+                    };
+
+                    double[] widths = new double[] {
+                        info.dimensions[2] / ratio,
+                        info.dimensions[0] / ratio,
+                        info.dimensions[2] / ratio,
+                        info.dimensions[0] / ratio,
+                        info.dimensions[0] / ratio,
+                        info.dimensions[0] / ratio
+                    };
+
+                    double[] heights = new double[] {
+                        info.dimensions[1] / ratio,
+                        info.dimensions[1] / ratio,
+                        info.dimensions[1] / ratio,
+                        info.dimensions[1] / ratio,
+                        info.dimensions[2] / ratio,
+                        info.dimensions[2] / ratio
+                    };
+
+                    for (int i = 0; i < 6; i++) {
+                        if(gMx >= posXs[i] && gMx <= posXs[i] + widths[i] && gMy >= posYs[i] && gMy <= posYs[i] + heights[i]) {
+                            mouseOverAny = true;
+                            alpha = 125;
+                            rMod = 0.5F;
+                            bMod = 1F;
+                            gMod = 0.25F;
+
+                            if(currentMouse && (diffX != 0 || diffY != 0) && info.identifier.equals(startingMouseDown)) {
+                                selectedIdentifiers.add(info.identifier);
+                            }
+
+                            if(currentMouse && !lastMouseDown) {
+                                startingMouseDown = info.identifier;
+                            }
+
+                            if(mouseDown) {
+                                if(!selectedIdentifiers.contains(info.identifier)) {
+                                    selectedIdentifiers.add(info.identifier);
+                                } else {
+                                    selectedIdentifiers.remove(info.identifier);
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+
+                    RendererHelper.drawColourOnScreen((int)(255*rMod), (int)(0*gMod), (int)(0*bMod), alpha, posXs[0], posYs[0], widths[0], heights[0], 0D);
+                    RendererHelper.drawColourOnScreen((int)(0*rMod), (int)(0*gMod), (int)(255*bMod), alpha, posXs[1], posYs[1], widths[1], heights[1], 0D);
+                    RendererHelper.drawColourOnScreen((int)(170*rMod), (int)(0*gMod), (int)(0*bMod), alpha, posXs[2], posYs[2], widths[2], heights[2], 0D);
+                    RendererHelper.drawColourOnScreen((int)(0*rMod), (int)(0*gMod), (int)(170*bMod), alpha, posXs[3], posYs[3], widths[3], heights[3], 0D);
+                    RendererHelper.drawColourOnScreen((int)(0*rMod), (int)(255*gMod), (int)(0*bMod), alpha, posXs[4], posYs[4], widths[4], heights[4], 0D);
+                    RendererHelper.drawColourOnScreen((int)(0*rMod), (int)(170*gMod), (int)(0*bMod), alpha, posXs[5], posYs[5], widths[5], heights[5], 0D);
                 }
+            }
+            if(mouseDown && !mouseOverAny) {
+                selectedIdentifiers.clear();
             }
 
             GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
@@ -129,7 +243,12 @@ public class WindowTexture extends Window
             {
                 workspace.getFontRenderer().drawString(I18n.translateToLocal("window.texture.remoteTexture"), posX + 4, posY + height - BORDER_SIZE - 12 - 20, Theme.getAsHex(workspace.currentTheme.font), false);
             }
+
+            lastMouseDown = currentMouse;
         }
+
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
     }
 
     @Override
